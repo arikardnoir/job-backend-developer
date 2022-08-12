edcaa69e1e0ea1c98cf5c8d74842dafa21c8762c
Aristote Lopes <a.lopes@vhsys.com.br>
2022-08-11 21:31:54 -0300

    creates migrations for productions

---
+++ database/migrations/2022_08_11_231818_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('products')) {
+            Schema::create('products', function (Blueprint $table) {
+                $table->id()->unique();
+                $table->string('name')->unique();
+                $table->decimal('price', 10, 2);
+                $table->text('description');
+                $table->string('category');
+                $table->text('image')->nullable()->default(null);
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
